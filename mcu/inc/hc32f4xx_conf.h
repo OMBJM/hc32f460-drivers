942848c5592364b6c5ccdbe51089a089efbc992f
KK <897086121@qq.com>
2026-05-05 16:08:34 +0800

    feat: 适配910的硬件, 目前这个提交不一定有用

--- mcu/inc/hc32f4xx_conf.h
+++ mcu/inc/hc32f4xx_conf.h
@@ -48,7 +48,7 @@ extern "C" {
  */
 #define LL_ICG_ENABLE              (DDL_ON)
 #define LL_UTILITY_ENABLE          (DDL_ON)
-#define LL_PRINT_ENABLE            (DDL_ON)
+#define LL_PRINT_ENABLE            (DDL_OFF)
 
 #define LL_ADC_ENABLE              (DDL_ON)
 #define LL_AES_ENABLE              (DDL_OFF)
